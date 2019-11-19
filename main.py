53d948a626d21dbcd53c00a04e7ee5b03312a642
Flachaire Nathan <uapv1700716@ecv-2w21a-ceri.univ-avignon.fr>
2019-11-19 18:59:31 +0100

    OHYES

--- main.py
+++ main.py
@@ -7,15 +7,29 @@ def getGridInfo(i,j):
 	print("j : ", j)
 
 def createCase(i, j):
+
+	pixel = PhotoImage(width=1, height=1)
+
 	case = Button()
+	case["text"] = "X"
+	case["image"] = pixel
 	case["command"] = lambda: getGridInfo(i,j)
 	case["bg"]= "blue"
+	case["height"] = 200
+	case["width"] = 200
 	if (i != 0 and i != 4 and j != 0 and j != 4):
 		case["state"] = DISABLED
-	case.grid(row=i,column=j)
+	case.grid(row=i,column=j, sticky="wens")
+
 
 
 root = Tk()
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+print(screen_width,screen_height)
+
 for i in range(0,5):
 	for j in range(0,5):
 		createCase(i,j)
