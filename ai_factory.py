0b333998f1a3d1870a8875f6cb910fb8d8670e80
AronGomu <nathan.flachaire@outlook.fr>
2019-12-22 14:52:02 +0100

    Final

---
+++ ai_factory.py
@@ -0,0 +1,13 @@
+import ai
+
+
+class AiFactory:
+
+    def __init__(self):
+        pass
+
+    def createAi(self, type):
+        if (type == "default"):
+            return ai.Ai()
+        else:
+            return ai.Ai()
